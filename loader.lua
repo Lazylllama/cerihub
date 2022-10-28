b6608411352650551bac3202e43edcf37d1d3166
Lazyllama <me@lazyllama.xyz>
2022-10-28 22:28:07 +0200

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,22 @@
+-- Cerihub by Lazyllama
+
+-- Credits: 
+-- Dev 1? : Lazyllama - https://Lazyllama.xyz
+-- Dev 2?: Exomeme - https://github.com/Exomeme
+
+-- Locals
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
+
+-- Windows
+local Window = Rayfield:CreateWindow({
+	Name = "Cerihub",
+	LoadingTitle = "Cerihub",
+	LoadingSubtitle = "Loading in...",
+	KeySystem = true, -- Set this to true to use our key system
+	KeySettings = {
+		Title = "Cerihub",
+		Subtitle = "Key System",
+		Note = "Add me on Dicord (Lazyllama#3006)",
+		Key = "LLAMA"
+	}
+	})
